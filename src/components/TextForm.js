78f000bff9fa9cb1ca6bc183591305b25b6fd241
Ishan <you@example.com>
2022-06-19 15:58:21 +1000

    added some styling to form

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,11 +7,11 @@ export const TextForm = function() {
         <form className='entire-form'>
 
             <label className='labels' id='top-line-label'>
-                <input type='text' />
+                <input type='text' placeholder='Top text'/>
             </label>
 
             <label className='labels' id='bottom-line-label'>
-                <input type='text' />
+                <input type='text' placeholder='Bottom text'/>
             </label>
 
             <button id='get-new-meme-btn'>Get a new meme image</button>
