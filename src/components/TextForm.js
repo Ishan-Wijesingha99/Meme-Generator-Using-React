b69a5f302350322be4f583ded687e05f4be53522
Ishan <you@example.com>
2022-06-23 17:20:08 +1000

    finished styling meme, form, and navbar to be responsive. Added state functionality to form

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,6 +14,10 @@ export const TextForm = function() {
     //     alert('You hovered over this button')
     // }
 
+
+
+    const [imageURL, getImageURL] = React.useState('');
+
     const getRandomMemeImage = function(e) {
         // prevent page reload
         e.preventDefault();
@@ -21,29 +25,35 @@ export const TextForm = function() {
         // create random number between 0 and 99
         let randomNumber = Math.floor(Math.random() * memesDataArray.data.memes.length);
         
-        console.log(memesDataArray.data.memes[randomNumber].url)
+        // based off random number, get a random meme URL
+        getImageURL(`${memesDataArray.data.memes[randomNumber].url}`)
+        console.log(imageURL);
+
     }
 
-
-
+    
 
     return (
-        <form className='entire-form'>
+        <div>
+            <form className='entire-form'>
 
-            <label className='labels' id='top-line-label'>
-                <input type='text' placeholder='Top text'/>
-            </label>
+                <label className='labels' id='top-line-label'>
+                    <input type='text' placeholder='Top text'/>
+                </label>
 
-            <label className='labels' id='bottom-line-label'>
-                <input type='text' placeholder='Bottom text'/>
-            </label>
+                <label className='labels' id='bottom-line-label'>
+                    <input type='text' placeholder='Bottom text'/>
+                </label>
 
-            {/* there is another way to add an event listener in javascript, and that's the onClick attribute in HTML, this is the main way we will add click events using React */}
-            {/* usually its onclick , but for JSX it's in camelcase, onClick */}
-            {/* you can add other events as well like onMouseEnter and onMouseLeave*/}
-            {/* we pass in a function that we want to execute once that event occurs */}
-            <button onClick={getRandomMemeImage} onMouseEnter={2} id='get-new-meme-btn'>Get a new meme image</button>
+                {/* there is another way to add an event listener in javascript, and that's the onClick attribute in HTML, this is the main way we will add click events using React */}
+                {/* usually its onclick , but for JSX it's in camelcase, onClick */}
+                {/* you can add other events as well like onMouseEnter and onMouseLeave*/}
+                {/* we pass in a function that we want to execute once that event occurs */}
+                <button onClick={getRandomMemeImage} id='get-new-meme-btn'>Get a new meme image</button>
 
-        </form>
+            </form>
+
+            <img id='meme-image' src={imageURL}/>
+        </div>
     )
 } 
